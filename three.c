3996f6179dc78a868931e99c81b138fb5aae28a7
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-10 11:01:25 +0530

    three second

--- three.c
+++ three.c
@@ -1,34 +1,28 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-int main ( ) {
-    
-    int n  = 111011 ;
-    bool isbinary = true;
+int main() {
 
-   if(n < 0){
-     isbinary = false;
-   }
-    
-   else{
-        while ( n > 0 ) {
-            
-            int lastdigit = n % 10 ;
+    int n = 111011;
 
-            if (lastdigit > 1){
-                isbinary = false;
-                break; 
-            }
-            
-            
-            n /= 10 ; 
+    if (n < 0) {
+        printf("Not a Binary Number");
+        return 0;
+    }
+
+    while (n > 0) {
+
+        int digit = n % 10;
+
+        if (digit != 0 && digit != 1) {
+            printf("Not a Binary Number");
+            return 0;
         }
-   }
 
-    if(isbinary){
-        printf("IT IS BINARY NUMBER");
+        n /= 10;
     }
-    else{
-        printf("it is not binary number");
-    }
+
+    printf("Binary Number");
+
+    return 0;
 }
