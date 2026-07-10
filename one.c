a555f6220e9c96f35a00cc5565f362dc7a2bc6d5
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-10 09:40:10 +0530

    first

---
+++ one.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+    
+    int n = 5 ;
+
+    int num = 1 ;
+    
+    for ( int i = 1 ; i <= n ; i++) {
+
+        printf("%d ",num);
+
+        num += i ; 
+
+    }
+
+    return 0;
+
+}
